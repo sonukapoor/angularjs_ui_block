3938614a54122a8acc32d8d00fe00171727ea49a
Sonu Kapoor <sonu.kapoor@rangle.io>
2016-11-18 10:53:43 -0500

    Changed JS scripts and added a new directive to load scripts into the head using append

--- script.js
+++ script.js
@@ -4,10 +4,20 @@ angular.module("plunker").directive('foo', function () {
   return {
     template: 'foo',
     restrict: 'E',
-    link: function postLink(scope) {
-      var qnasrc1 = '<link rel="stylesheet" href="//static.www.turnto.com/tra4_3/tra.css" /><script type="text/javascript" src="/js/cookie_fn.js?v=08132013" async></script><script type="text/javascript" src="/js/turnto_vs.js" async></script><script type="text/javascript">var turnToConfig = {siteKey: "I1h5bZdr3OSP4fMsite",skipCssLoad:true,setupType: "dynamicEmbed",registration : { localGetLoginStatusFunction : localGetLoginStatusFunction,localRegistrationUrl : "/p/gadgets/turntoLogin.jsp", localGetUserInfoFunction : localGetUserInfoFunction,localLogoutFunction : localLogoutFunction}};window.onload= function(){var tt = document.createElement("script"); tt.type = "text/javascript"; tt.src = document.location.protocol + "//static.www.turnto.com/sitedata/I1h5bZdr3OSP4fMsite/v4_3/' + 123 + '/d/itemjs";var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(tt, s);var tt = document.createElement("script"); tt.type = "text/javascript"; tt.async = true;tt.src = document.location.protocol + "//static.www.turnto.com/traServer4_3/trajs/" + turnToConfig.siteKey + "/tra.js";var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(tt, s);};'
-      var qnasrc2 = '<script>var TurnToItemSku = "' + 123 + '";var itemjsUrl = "//static.www.turnto.com/sitedata/I1h5bZdr3OSP4fMsite/v4_3/' + 123 + '/d/itemjs";</script>';
-      scope.qna1 = qnasrc1;
+    link: function ($scope) {
+      var qnasrc1 = '<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/emojione/1.3.0/lib/js/emojione.min.js" async></script>';
+      $scope.qna1 = qnasrc1;
+    }
+  }
+});
+
+angular.module("plunker").directive('includeScriptInHead', function () {
+  return {
+    restrict: 'A',
+    link: function ($scope, $el) {
+      var script = document.createElement('script');
+      script.src = 'https://cdnjs.cloudflare.com/ajax/libs/emojione/2.2.6/lib/js/emojione.min.js'
+      document.head.appendChild(script);
     }
   }
 });
